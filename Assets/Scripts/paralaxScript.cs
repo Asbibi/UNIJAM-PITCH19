3889a36da02e0b5f3a55f9b65849fab3aa45c6b1
Boneoc01 <rynkiewicz.paul@gmail.com>
2020-11-15 13:29:32 +0100

    Ajout FinalScene (please nikrien)

---
+++ Assets/Scripts/paralaxScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paralaxScript : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float dist = (camera.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
